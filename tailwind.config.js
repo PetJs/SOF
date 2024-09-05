99d6519f0b9ebc9aaff791421f3b4718ee2a93aa
e-jessie <140353139+e-jessie@users.noreply.github.com>
2024-09-04 23:28:01 -0800

    finished up the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['YourCustomFont', 'system-ui', 'sans-serif'],
+        serif: ['YourSerifFont', 'serif'],
+        mono: ['YourMonoFont', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
