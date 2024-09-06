dd9f739a4e7bc430b349dea7981c4b65c43b9fee
e-jessie <140353139+e-jessie@users.noreply.github.com>
2024-09-06 01:24:52 -0800

    added some styling and linked the two pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import SuggestionsPage from './components/suggestions-page'
 import { RecipeContextProvider } from './components/suggestions'
+import LandingPage from './components/landingpage'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
 function App() {
 
   return (
     <>
-      <RecipeContextProvider>
+      <Router>
+        <RecipeContextProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/suggestions" element={<SuggestionsPage />} />
+          </Routes>
+        </RecipeContextProvider>
+      </Router>
+      {/*<RecipeContextProvider>
         <SuggestionsPage />
-      </RecipeContextProvider>
+      </RecipeContextProvider>*/}
     </>
-  )
+  );
 }
 
 export default App
